31af1a32a0a0b409674b08c7bc4ff4fbeb59a029
Zhansaya Nukabai <dastan161098@gmail.com>
2026-02-27 22:33:30 +0500

    Python iterators, generators, dates, math, and JSON

--- Practice 4/json.py
+++ Practice 4/json.py
@@ -71,3 +71,24 @@ json.dumps(x, indent=4, separators=(". ", " = "))
 
 #7
 json.dumps(x, indent=4, sort_keys=True)
+
+#EXERCISES
+import json
+
+with open("sample-data.json", "r") as f:
+    data = json.load(f)
+
+print("Interface Status")
+print("=" * 80)
+print(f"{'DN':60} {'Description':15} {'Speed':10} {'MTU':5}")
+print("-" * 80)
+
+for item in data["imdata"]:
+    attributes = item["l1PhysIf"]["attributes"]
+
+    dn = attributes["dn"]
+    descr = attributes["descr"]
+    speed = attributes["speed"]
+    mtu = attributes["mtu"]
+
+    print(f"{dn:60} {descr:15} {speed:10} {mtu:5}")
